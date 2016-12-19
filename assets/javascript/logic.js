58ce2a095437b75b0c7bc0b12ab81d50112c9bad
maxvanbel <maxvanbel@gmail.com>
2016-12-18 23:29:52 -0700

    more logic game function

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -47,6 +47,62 @@ function startGame () {
 	console.log(blanksAndSucesses);
 
 } 
-// MAIN PROCESS 
+
+function checkLetters(letter) {
+	// Check if letter exists in code at all
+	var isLetterInWord = false;
+
+	for (var i = 0; i < numBlanks; i++) {
+		if(selectedWord[i] == letter) {
+			isLetterInWord = true;
+		}
+	}	
+	// Check where in the word the letter exists, then populate out the blanksAndSuccesses array 
+	if(isLetterInWord) {
+		for (var j = 0; j < numBlanks; j++) {
+			if(selectedWord[j] == letter) {
+				blanksAndSucesses[j] = letter;
+			}
+		}
+	}
+	// Letter was not found
+	else {
+		wrongLetters.push(letter);
+		guessesLeft--;
+	}	
+	// Testing / debugging 
+}
+
+function roundComplete() {
+	console.log("Win Count: " + winCount + "| Loss Count: " + lossCount + "| Guesses Left: " + guessesLeft);
+
+	// Check if user won
+	if(lettersInWord.toString() == blanksAndSucesses.toString()) {
+		winCount++;
+		alert("You Won!");
+
+		// Update the win counter in the HTML
+		document.getElementById("winCounter").innerHTML = winCount;
+		startGame();
+	}
+
+	// Check if user lost
+
+
+}
+// MAIN PROCESS p
 // -----------------------------------------------------------------------
+
+// Initiates the code the first time
 startGame(); 
+
+// Register Keyclicks
+
+document.onkeyup = function(event) {
+	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	checkLetters(letterGuessed);
+	roundComplete();
+
+	// Testing and Debugging	
+	console.log(letterGuessed);
+};
